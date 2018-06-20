e5b6e5e298fbdaa247a019c13a6b8828074c2158
Mikaela Scornaienchi <scornaim@gmail.com>
2018-06-20 14:30:45 -0400

    pushing

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -5,145 +5,86 @@ import ReactDOM from 'react-dom';
 // rows colums and squares need to all have different numbers 
   // set the state to an empty array and then push in the numbers as the user enters them to monitor if the squares and columns all have different numbers, 
 
+const sudoku = [
+  [0, 0, 3, 0, 2, 0, 6, 0, 0],
+  [9, 0, 0, 3, 0, 5, 0, 0, 1],
+  [0, 0, 1, 8, 0, 6, 4, 0, 0],
+  [0, 0, 8, 1, 0, 2, 9, 0, 0],
+  [7, 0, 0, 0, 0, 0, 0, 0, 8],
+  [0, 0, 6, 7, 0, 8, 2, 0, 0],
+  [8, 0, 2, 6, 0, 9, 5, 0, 0],
+  [8, 0, 0, 2, 0, 3, 0, 0, 9],
+  [0, 0, 5, 0, 1, 0, 3, 0, 0],
+]
+
+const cell = []
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state({
-      
+    this.state ({ 
+      board: ''
     })
-
+    this.gameBoard = this.gameBoard.bind(this);
   }
 
-  
+  // a = x axis
+  // b = y axis
+  gameBoard() {
+    let a = 0;
+    let b = 0;
+    for (let i = 0; i < 81 ; i ++) {
+      cell[i] = new cell(a, b, i, sudoku[i])
+      a = a + 50;
+      if (i % 9 == 0 && i !== 0) {
+        b = b + 50;
+        a = 0;
+      }
+      console.log(sudoku[i]);
+    }
+    this.setState({
+      board: call[i]
+    })
+  }
+
+  draw() {
+    background(51);
+    for (let i = 0; i < 81; i++) {
+      cell[i].show();
+    }
+  }
+
+  // cell(a,b,i,num) {
+  //   this.a = a;
+  //   this.b = b;
+  //   this.i = i;
+  //   this.num = num;
+  //   // the border 
+  //   this.show = () => {
+  //     noFill();
+  //     stroke(255);
+  //     rec(this.a, this.b, 50, 50);
+  //     fill(255);
+  //     textSize(32);
+  //   }
+  // }
 
   render() {
     return (
       <div>
-        <form action="">
-          <div>
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-          </div>
-          <div>
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-          </div>
-          <div>
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-          </div>
-          <div>
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-          </div>
-          <div>
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-          </div>
-          <div>
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-          </div>
-          <div>
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-          </div>
-          <div>
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-          </div>
-          <div>
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-          </div>
-          <div>
-            <label type="text" disabled={true} value={this.state.userSelect}/>
-            <label type="text" disabled={true} value={this.state.userSelect}/>
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-            <label type="text" disabled={true} value={this.state.userSelect} />
-          </div>
-        </form>
-
-        <div className="numbers">
-          <button label={'1'} onClick={this.userInput} type='action' />
-          <button label={'2'} onClick={this.userInput} type='action' />
-          <button label={'3'} onClick={this.userInput} type='action' />
-          <button label={'4'} onClick={this.userInput} type='action' />
-          <button label={'5'} onClick={this.userInput} type='action' />
-          <button label={'6'} onClick={this.userInput} type='action' />
-          <button label={'7'} onClick={this.userInput} type='action' />
-          <button label={'8'} onClick={this.userInput} type='action' />
-          <button label={'9'} onClick={this.userInput} type='action' />
+        
+        <div>
+          {/* <label display={this.board}></label> */}
+          {this.state.gameBoard.map((number) => {
+            console.log(number);
+            return (
+              <div className="number">
+                {number}
+                </div>
+            )
+          })}
         </div>
+
       </div>
     )
   }
